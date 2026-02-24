93532f1b670a821852282671935980470b8c96f0
SandakalumSilva <sandakalum1996@gmail.com>
2026-02-24 06:36:30 +0530

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html class=''>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name="robots" content="noindex">
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
+    <link rel='stylesheet prefetch'
+        href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
+
+
+    <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    {{ $slot }}
+
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+        integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="module" src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/lineSpinner.js"></script>
+    {{ $scripts ?? '' }}
+</body>
+
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,54 @@
+<div id="sidepanel">
+    <div id="profile">
+        <div class="wrap">
+            <img id="profile-img" src="{{ asset('default-image/avatar.jpg') }}" class="online" alt="" />
+            <p>{{ auth()->user()->name }}</p>
+            <i class="fa fa-chevron-down expand-button" aria-hidden="true"></i>
+            <div id="status-options">
+                <ul>
+                    <li id="status-online" class="active"><span class="status-circle"></span>
+                        <p>Online</p>
+                    </li>
+                    <li id="status-away"><span class="status-circle"></span>
+                        <p>Away</p>
+                    </li>
+                    <li id="status-busy"><span class="status-circle"></span>
+                        <p>Busy</p>
+                    </li>
+                    <li id="status-offline"><span class="status-circle"></span>
+                        <p>Offline</p>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    <hr>
+    <div id="contacts">
+        <ul>
+            @forelse ($users as $user)
+                <li class="contact">
+                    <div class="wrap">
+                        <span class="contact-status online"></span>
+                        <img src="{{ asset('default-image/avatar.jpg') }}" alt="" />
+                        <div class="meta">
+                            <p class="name">{{ $user->name }}</p>
+                            <p class="preview">{{ $user->email }}</p>
+                        </div>
+                    </div>
+                </li>
+            @empty
+                <p class="text-center">No users found.</p>
+            @endforelse
+
+        </ul>
+    </div>
+
+    <div class="text-center">
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit" class="btn btn-danger">Logout</button>
+        </form>
+    </div>
+
+</div>
